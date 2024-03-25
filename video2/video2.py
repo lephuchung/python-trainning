398d6445007e82a13a8ec0e96bb435604c0869de
lephuchung <lephuchung003@gmail.com>
2024-03-26 01:57:18 +0700

    video2

---
+++ video2/video2.py
@@ -0,0 +1,8 @@
+character_name = "Hưng"
+character_age = 21
+# check = True
+check = False
+if check:
+    print(f"Đây là {character_name}, anh ấy {character_age} tuổi")
+else: print(f'Đây không phải {character_name}')
+
