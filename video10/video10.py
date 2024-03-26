befdf13201d2435598f1d20446eaac13e55bf196
lephuchung <lephuchung003@gmail.com>
2024-03-26 20:04:12 +0700

    video10

---
+++ video10/video10.py
@@ -0,0 +1,10 @@
+# Cau truc du lieu Tuple
+# Tuple tuong tu nhu danh sach
+# Tuple khong cho phep thay doi cac gia tri ben trong
+# Tuple khong cho phep them gia tri
+coordinate = (123, 456)
+haha = [(1,2), (3,4)]
+
+print(coordinate[1])
+print(coordinate[0])
+print(haha[1])
