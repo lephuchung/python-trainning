f28a20990b8fb3e1e4a1f29635cef76b313a9df3
lephuchung <lephuchung003@gmail.com>
2024-03-26 17:18:11 +0700

    video4

---
+++ video4/video4.py
@@ -0,0 +1,17 @@
+import math
+
+a = 2
+b = 3
+c = 2.4
+d = 5.6
+e = -2
+print('check a + b =', a + b)
+print('check b % a =', b % a)
+print('check b / a =', b / a)
+print('check c =', c)
+print('check e =', e)
+print('check sqrt(2a) =',math.sqrt(2 * a))
+print('check abs(e) =',abs(e))
+print('check round(c) =', round(c)) # Làm tròn auto
+print('check floor(d) =', math.floor(d)) # Làm tròn xuống
+print('check ceil(c) =', math.ceil(c)) # Làm tròn lên
