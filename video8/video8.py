8218b1bd2798784943852777603624c10a2240b3
lephuchung <lephuchung003@gmail.com>
2024-03-26 19:03:53 +0700

    video8

---
+++ video8/video8.py
@@ -0,0 +1,11 @@
+# Du lieu dang danh sach (dang mang)
+
+teams = ['Barcelona', 'Real Madrid', 'Manchester City', 'Chelsea', 'Liverpool']
+print(teams)
+print(teams[1]) # In phan tu o vi tri 1
+print(teams[1:]) # In phan tu tu vi tri so 1 den cuoi danh sach
+print(teams[1:4]) # In phan tu tu vi tri so 1 den vi tri so 3
+print(teams[-1]) # In phan tu tu cuoi mang
+# Chu y: Index tinh tu cuoi mang ve dau mang se la -1 -2 -3 -4....
+teams[1]='Manchester United'
+print(teams)
